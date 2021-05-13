b78fd036f04451c45713de8ecb51f3234137d8ac
Роман Квітницький <Kvitnytskyi.R@ms.sumdu.edu.ua>
2021-05-13 14:07:41 +0300

    Add project files.

---
+++ Optimal_Asset_Allocation_final/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Optimal_Asset_Allocation_final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnTable = new System.Windows.Forms.Button();
+            this.money = new System.Windows.Forms.NumericUpDown();
+            this.nEnt = new System.Windows.Forms.NumericUpDown();
+            this.nAtt = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.money)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nEnt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(261, 24);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(527, 317);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(146, 347);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 47);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Кількість підприємств:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Кількість варіантів вкладення:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Сума вкладення:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnTable
+            // 
+            this.btnTable.Location = new System.Drawing.Point(12, 347);
+            this.btnTable.Name = "btnTable";
+            this.btnTable.Size = new System.Drawing.Size(91, 47);
+            this.btnTable.TabIndex = 5;
+            this.btnTable.Text = "Створити таблицю";
+            this.btnTable.UseVisualStyleBackColor = true;
+            this.btnTable.Click += new System.EventHandler(this.btnTable_Click);
+            // 
+            // money
+            // 
+            this.money.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.money.Location = new System.Drawing.Point(197, 144);
+            this.money.Maximum = new decimal(new int[] {
+            99999999,
+            0,
+            0,
+            0});
+            this.money.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.money.Name = "money";
+            this.money.Size = new System.Drawing.Size(40, 20);
+            this.money.TabIndex = 6;
+            this.money.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.money.ValueChanged += new System.EventHandler(this.money_ValueChanged);
+            // 
+            // nEnt
+            // 
+            this.nEnt.Location = new System.Drawing.Point(197, 182);
+            this.nEnt.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nEnt.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nEnt.Name = "nEnt";
+            this.nEnt.Size = new System.Drawing.Size(40, 20);
+            this.nEnt.TabIndex = 7;
+            this.nEnt.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nEnt.ValueChanged += new System.EventHandler(this.nEnt_ValueChanged);
+            // 
+            // nAtt
+            // 
+            this.nAtt.Location = new System.Drawing.Point(197, 222);
+            this.nAtt.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nAtt.Name = "nAtt";
+            this.nAtt.Size = new System.Drawing.Size(40, 20);
+            this.nAtt.TabIndex = 8;
+            this.nAtt.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nAtt.ValueChanged += new System.EventHandler(this.nAtt_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nAtt);
+            this.Controls.Add(this.nEnt);
+            this.Controls.Add(this.money);
+            this.Controls.Add(this.btnTable);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Optimal Asset Allocation";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.money)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nEnt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTable;
+        private System.Windows.Forms.NumericUpDown money;
+        private System.Windows.Forms.NumericUpDown nEnt;
+        private System.Windows.Forms.NumericUpDown nAtt;
+    }
+}
+
---
+++ Optimal_Asset_Allocation_final/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Optimal_Asset_Allocation_final
+{
+    public partial class Form1 : Form
+    {
+        int factCount;
+        int varCount;
+        int moneyCount;
+        string[] plan;
+        string[] newPlan;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) // основний розрахунок
+        {
+            try
+            {
+                int[] attachments = new int[varCount]; //Варианты вложений
+                double[] Fn = new double[varCount]; // введенные значения предприятий
+                double[] fn = new double[varCount]; //функции предприятий 
+                double[,] plus = new double[varCount, varCount];
+
+                plan = new string[varCount];
+                newPlan = new string[varCount];
+
+                for (int i = 0; i < attachments.Length; i++) //запись вариантов вложений
+                    attachments[i] = Convert.ToInt32(dataGridView.Rows[i].Cells[0].Value);
+
+                for (int i = 0; i < varCount; i++) //запись начального Fn
+                    Fn[i] = Convert.ToInt32(dataGridView.Rows[i].Cells[1].Value);
+
+                for (int fact = 0; fact < factCount - 1; fact++) //итерации вычислениый
+                {
+
+                    for (int i = 0; i < varCount; i++) //запись начального fn
+                        fn[i] = Convert.ToInt32(dataGridView.Rows[i].Cells[fact + 2].Value);
+
+                    for (int i = 0; i < varCount; i++) //сложение элементов на пересечении 
+                        for (int j = 0; j < varCount; j++)
+                            if (attachments[varCount - 1] >= (attachments[i] + attachments[j])) plus[i, j] = fn[i] + Fn[j];
+
+                    Array.Copy(newPlan, plan, plan.Length);
+
+                    for (int var = 0; var < varCount; var++)
+                    {
+                        int variant = attachments[var];
+                        double max = 0;
+                        int x = 0, y = 0;
+
+                        for (int i = 0; i < varCount; i++)
+                            for (int j = 0; j < varCount; j++)
+                                if (attachments[j] + attachments[i] == variant && plus[i, j] > max)
+                                {
+                                    max = plus[i, j];
+                                    x = i;
+                                    y = j;
+                                }
+
+                        Fn[var] = plus[x, y];
+
+                        if (fact == 0)
+                            newPlan[var] = attachments[y].ToString() + " " + attachments[x].ToString();
+                        else
+                            newPlan[var] = plan[y] + " " + attachments[x].ToString();
+                    }
+                }
+
+                double max1 = 0;
+                int x1 = 0;
+
+                string res = "";
+
+                for (int i = 0; i < varCount; i++)
+                    if (Fn[i] > max1)
+                    {
+                        max1 = Fn[i];
+                        x1 = i;
+                    }
+
+                int kolRes = 0;
+
+                for (int i = 0; i < dataGridView.RowCount; i++)
+                    if (Convert.ToInt32(dataGridView.Rows[i].Cells[0].Value) != 0) kolRes++;
+
+                res = "Найбільший пріріст, при " + Convert.ToString(kolRes) + " варіантів кладення між " + factCount.ToString();
+
+                if (factCount <= 4)
+                    res += " -ма підприємствами, складе " + Fn[x1].ToString() + "\n";
+                else
+                    res += " -ю підприємствами, складе " + Fn[x1].ToString() + "\n";
+                for (int i = 0; i < factCount; i++)
+                    res += (i + 1).ToString() + "-му підприємству виділити " + newPlan[x1].Split(' ')[i] + "\n";
+                MessageBox.Show(res, "Відповідь", MessageBoxButtons.OK);
+            }
+            catch (Exception) { }
+        }
+
+        private void btnTable_Click(object sender, EventArgs e) // створення таблиці
+        {
+            if (nEnt.Value > 1) btnStart.Enabled = true;
+            else btnStart.Enabled = false;
+
+            int moneyPart = 0;
+
+            try
+            {
+                factCount = Convert.ToInt32(nEnt.Value);
+                varCount = (Convert.ToInt32(nAtt.Value) + 1);
+
+                if (money.Text == "") moneyPart = 1;
+                else
+                {
+                    moneyCount = Convert.ToInt32(money.Text);
+                    moneyPart = moneyCount / (varCount - 1);
+                }
+
+                dataGridView.Rows.Clear();
+                dataGridView.Columns.Clear();
+
+                DataGridViewColumn col1 = new DataGridViewTextBoxColumn();
+                col1.HeaderText = "Вкладені кошти";
+                dataGridView.Columns.Add(col1);
+
+                for (int i = 1; i < factCount + 1; i++)
+                {
+                    DataGridViewColumn col = new DataGridViewTextBoxColumn();
+                    col.HeaderText = "Підпр." + i.ToString();
+                    dataGridView.Columns.Add(col);
+                }
+
+                for (int i = 0; i < varCount; i++)
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    dataGridView.Rows.Add(row);
+                    dataGridView.Rows[i].Cells[0].Value = (i * moneyPart).ToString();
+                }
+
+            }
+            catch (Exception ex) { System.Console.WriteLine(ex.Data.ToString()); }
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Ви дійсно хочете вийти з програми?",
+             "Завершення програми",
+             MessageBoxButtons.YesNo,
+             MessageBoxIcon.Warning
+            );
+
+            if (dialog == DialogResult.Yes) e.Cancel = false;
+            else e.Cancel = true;
+        }
+
+        private void grid_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            TextBox tb = (TextBox)e.Control;
+
+            if (dataGridView.CurrentCell.ColumnIndex >= 0)
+                tb.KeyPress += new KeyPressEventHandler(tb_KeyPress);
+            else
+                tb.KeyPress -= tb_KeyPress;
+        }
+        void tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!Char.IsNumber(e.KeyChar) && (e.KeyChar != '-') && (e.KeyChar != ',')))
+                if ((e.KeyChar != (char)Keys.Back) || (e.KeyChar != (char)Keys.Delete)) e.Handled = true;
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void money_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nEnt_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nAtt_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
